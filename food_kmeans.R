899602c89e07343cb78eb225dd05989ff8b68d3c
Leila Cielok <leila.cielok@studenti.unimi.it>
2025-02-14 12:32:32 +0100

    Adding comments for method including outlier

--- food_kmeans.R
+++ food_kmeans.R
@@ -14,19 +14,13 @@ Food_Production <- Food_Production %>%
          animal_feed = 'Animal.Feed',
   )
 
+# Scale data
 scaled_data <- Food_Production
 scaled_data[, -1] <- scale(Food_Production[, -1])
 scaled_data <- scaled_data[, 2:8]
 
-set.seed(123)
-k.means.fit <- kmeans(scaled_data, 4)
-str(k.means.fit)
-scaled_data$cluster <- k.means.fit$cluster
-table(k.means.fit$cluster, scaled_data$cluster)
-
-
-
-wssplot <- function(data, nc=15, seed=1234){
+# Find optimal number of clusters
+wssplot <- function(data, nc=15, seed=123){
   wss <- (nrow(data)-1)*sum(apply(data,2,var))
   for (i in 2:nc){
     set.seed(123)
@@ -36,15 +30,24 @@ wssplot <- function(data, nc=15, seed=1234){
 
 wssplot(scaled_data, nc=10)
 
+"It looks from the plot that 4 is the optimal number of cluster"
+
+set.seed(123)
+k.means.fit <- kmeans(scaled_data, 4)
+str(k.means.fit)
+scaled_data$cluster <- k.means.fit$cluster
+table(k.means.fit$cluster, scaled_data$cluster)
+
+"From the table of the k-means fit it, we find out that the second cluster has only three observations.
+It is probably more convenient to try with three clusters."
+
 clusplot(scaled_data, k.means.fit$cluster, 
          main='2D representation of the Cluster solution',
          color=TRUE, shade=TRUE,
          labels=2, lines=0)
 
-# go through all the plots
-for(k in 2:6){
-  k.m.fit <- kmeans(scaled_data, k)
-  clusplot(scaled_data, k.m.fit$cluster, 
-           main=sprintf("2D representation of the Cluster solution\n k = %d",k),
-           color=TRUE, shade=TRUE,
-           labels=2, lines=0)}
+"We can see from the plot that the second cluster that is introduced when the number is set to 4,
+is probably due to the presence of the outlier 34.
+We can try to apply the k-median method, or re-try the k-means after dropping the outlier."
+
+
