13b456faf91c8dce0284324cb6f291767aedc7a9
Leila Cielok <leila.cielok@studenti.unimi.it>
2025-02-17 09:27:16 +0100

    Add comments and explanations to our code: tried hierarchical clustering and then HCPC to obtain a robust result: not optimal method.

--- food_HCPC.R
+++ food_HCPC.R
@@ -3,22 +3,25 @@ library(dplyr)
 library(FactoMineR)
 library(factoextra)
 
+# Download dataset
 Food_Production <- read.csv("Food_Production.csv")
-Food_Production <- as.data.frame(Food_Production)
 
+# Rename variables
 Food_Production <- Food_Production %>%
   rename(food_product = 'Food.product',
          land_use_change = 'Land.use.change',
          animal_feed = 'Animal.Feed',
   )
 
-
+# Scale the data (except first column) and select variables until 8th.
 scaled_data <- Food_Production
 scaled_data[, -1] <- scale(Food_Production[, -1])
 scaled_data <- scaled_data[, 2:8]
 
-##################################
-# SALINI CODE
+
+## HIERARCHICAL CLUSTERING
+# Compare the results of hierarchical clustering based on average, complete and ward linkage methods.
+# Build the dendograms for each method
 
 #Avg linkage plot
 h1<-hclust(dist(scaled_data), method="average")
@@ -35,54 +38,54 @@ h3<-hclust(dist(scaled_data), method="ward.D2")
 plot(h3, main="Ward linkage")
 rect.hclust(h3, 4)
 
-# Try with 4 clusters (after trying the table method we see that Ward needs only 2) 
+# Try with 4 clusters 
+# After trying the table method we see that Ward needs only 2
+
 average <- cutree(h1, k=4)
 complete<- cutree(h2, k=4)
-ward<- cutree(h3, k=2)
+ward<- cutree(h3, k=4)
+
+# Compare the linkage methods in pairs
 table(average,complete)
 table(average, ward)
 table(complete, ward)
 table(average, complete)
-dev.off()
 
 # Visualize all three plots together
 opar <- par(mfrow = c(1, 3))   
 plot(h1, main="average linkage")
 plot(h2, main="complete linkage")
 plot(h3, main="Ward linkage")
-dev.off()
 
 # Explore solutions
-h1cluster <- cutree(h1, k=4)
-h2cluster <- cutree(h2, k=4)
-h3cluster <- cutree(h3, k=2)
-plot(scaled_data[,c(1,2,3,4)], col=h1cluster, main="average linkage")
-plot(scaled_data[,c(1,2,3,4)], col=h2cluster, main="complete linkage")
-plot(scaled_data[,c(1,2,3,4)], col=h3cluster, main="ward linkage")
+plot(scaled_data[,c(1,2,3,4)], col=average, main="average linkage")
+plot(scaled_data[,c(1,2,3,4)], col=complete, main="complete linkage")
+plot(scaled_data[,c(1,2,3,4)], col=ward, main="ward linkage")
 
-table(h1cluster)
-table(h2cluster)
-table(h3cluster)
+table(average)
+table(complete)
+table(ward)
 
-# Group means
-medie1<-aggregate(scaled_data, list(h1cluster), mean)
-t(round(medie, 2))
-medie2<-aggregate(scale(scaled_data), list(h2cluster), mean)
-t(round(mediez, 2))
-medie3<-aggregate(scale(scaled_data), list(h3cluster), mean)
-t(round(mediez, 2))
+# Cluster means
+means1<-aggregate(scaled_data, list(average), mean)
+t(round(means1, 2))
+means2<-aggregate(scale(scaled_data), list(complete), mean)
+t(round(means2, 2))
+means3<-aggregate(scale(scaled_data), list(ward), mean)
+t(round(means3, 2))
 
 "The first clusters includes the majority of the data, while the others have very few.
 The hierarchical method based on the Ward linkage looks more informative, but the number
 of clusters obtained (that we can see from the table) is only 2: increasing it to three, 
 only one value is assigned to the additional cluster."
 
-##########################################
 
+## PRINCIPAL COMPONENT ANALYSIS
 
-pca_res <- prcomp(scaled_data[, 2:8], scale. = TRUE)
+# PCA on scaled data
+pca_res <- prcomp(scaled_data[, 1:7], scale. = TRUE)
 
-# Visualize variables only
+# Visualize variables only to see which contribute more to the PC variance 
 fviz_pca_var(
   pca_res,
   col.var = "contrib",
@@ -90,33 +93,45 @@ fviz_pca_var(
   repel = TRUE
 )
 
+# How much variance is explained by each component 
 summary(pca_res)
+# Show the data projected on the first two PC
 fviz_pca_ind(pca_res)
 
-# hierarchical clustering
-<<<<<<< Updated upstream:food_HCPC.R
-Food_Production <- Food_Production[sapply(Food_Production, is.numeric)]
-pca_res <- PCA(Food_Production[, 2:8], graph = FALSE) #NA changed with the mean
-res.hcpc <- HCPC(pca_res, graph = FALSE)
-=======
-res.hcpc <- HCPC(scaled_data, graph = FALSE)
->>>>>>> Stashed changes:food_code.R
 
-# Number of clusters chosen
+## HCPC
+# Hierarchical clustering on Principal Components
+# Try to combine PCA and hierarchical clustering to see if we get better results
+
+# Remove non-numeric columns
+Food_Production <- Food_Production[sapply(Food_Production, is.numeric)]
+
+# PCA (most suitable method for HCPC)
+pca_res <- PCA(Food_Production[, 2:8], graph = FALSE) #NA changed with the mean
+
+# Combine PCA and hierarchical clustering
+res.hcpc <- HCPC(pca_res, graph = FALSE)
+
+# Number of clusters to choose
+# We get 7 clusters
 res.hcpc$call$t$nb.clust
 
 # Visualize dendogram
 plot(res.hcpc, choice = "tree")
 
-# Visualize clusters in factor map
+# Visualize clusters in factor map: distribution of data wrt the PC
 plot(res.hcpc, choice = "map")
 
-<<<<<<< Updated upstream:food_HCPC.R
-centroids <- res.hcpc$desc.var$quanti
-print(centroids)
-=======
-# Centri dei cluster (medie delle variabili per ciascun cluster)
+# Extract centroids (mean for each cluster)
 centroids <- res.hcpc$desc.var$quanti
 print(centroids)
 
->>>>>>> Stashed changes:food_code.R
+# Variable means for each cluster
+# We see that clusters 2, 3, 4, 5 include only one observation: this method is not optimal.
+centroids <- res.hcpc$desc.var$quanti
+print(centroids)
+
+
+"We started by applying the hierarchical clustering method on scaled data.
+After that, we used PCA to reduce data dimensionality.
+Eventually, we combined PCA and clustering through the HCPC, to obtain a robust solution."
