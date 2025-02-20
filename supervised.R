2e4078b03957f1b0a6b0c975410d77d45d45d482
Paolo-Minini <paolo.minini@studenti.unimi.it>
2025-02-20 17:00:27 +0100

    logistic first-try

--- supervised.R
+++ supervised.R
@@ -1,33 +1,36 @@
-# Installare i pacchetti (se non già installati)
-install.packages("caret")
-install.packages("dplyr")
-
 # Caricare le librerie
 library(caret)
 library(dplyr)
 library(tidyverse)
 
 
-
 df <- read.csv("bankmarketing/bank.csv", sep = ';')
-df[df == "unknown"] <- NA
-df[df == "-1"] <- NA
 
-# Convertire la variabile target "y" in numerica (0 = no, 1 = yes)
-df$y <- ifelse(df$y == "yes", 1, 0)
+df <- df %>%
+  mutate(
+    # Binary variables (convert yes/no to 1/0)
+    default = ifelse(default == "yes", 1, 0),
+    housing = ifelse(housing == "yes", 1, 0),
+    loan = ifelse(loan == "yes", 1, 0),
+    y = ifelse(y == "yes", 1, 0),
+    
+    # Transform pdays into two variables
+    contacted_before = ifelse(pdays > -1, 1, 0),
+  ) %>%
+  select(-pdays) # Remove original pdays column
 
+# One-hot encoding for categorical variables
+df <- dummyVars("~ .", data = df, fullRank = TRUE) %>% 
+  predict(df) %>%
+  as.data.frame()
 
+# Normalize numeric columns (age, balance, duration, campaign, previous)
+numeric_cols <- c("age", "balance", "duration", "campaign", "previous")
+df[numeric_cols] <- scale(df[numeric_cols])
 
-# Identificare le colonne categoriche
-categorical_cols <- c("job", "marital", "education", "default", "housing", "loan", "contact", "month", "poutcome")
+# Creare il modello di regressione logistica
+logistic_model <- glm(y ~ ., data = df, family = binomial())
 
-# Creare le variabili dummy (One-Hot Encoding)
-dummy_model <- dummyVars(~ ., data=df, fullRank=TRUE)  # fullRank=TRUE per evitare collinearità
-df_encoded <- predict(dummy_model, newdata=df)
-
-# Convertire in dataframe
-df_encoded <- as.data.frame(df_encoded)
-
-# Visualizzare le prime righe
-head(df_encoded)
+# Mostrare il sommario del modello
+summary(logistic_model)
 
