53b679b3ec56d499f6b4e855c4cdb7889158091c
Francesco <francescosebastiano.memmola@studenti.unimi.it>
2025-02-20 17:42:33 +0100

    Update supervised.R

--- supervised.R
+++ supervised.R
@@ -1,36 +1,69 @@
 # Caricare le librerie
 library(caret)
 library(dplyr)
-library(tidyverse)
+library(ggplot2)
+library(pROC)
 
-
+# Leggere il dataset
 df <- read.csv("bankmarketing/bank.csv", sep = ';')
 
+# Convertire variabili binarie (yes/no -> 1/0)
 df <- df %>%
   mutate(
-    # Binary variables (convert yes/no to 1/0)
     default = ifelse(default == "yes", 1, 0),
     housing = ifelse(housing == "yes", 1, 0),
     loan = ifelse(loan == "yes", 1, 0),
     y = ifelse(y == "yes", 1, 0),
-    
-    # Transform pdays into two variables
-    contacted_before = ifelse(pdays > -1, 1, 0),
+    contacted_before = ifelse(pdays > -1, 1, 0)
   ) %>%
-  select(-pdays) # Remove original pdays column
+  select(-pdays)
 
-# One-hot encoding for categorical variables
-df <- dummyVars("~ .", data = df, fullRank = TRUE) %>% 
-  predict(df) %>%
-  as.data.frame()
+# One-hot encoding per variabili categoriche
+dummies <- dummyVars("~ .", data = df, fullRank = TRUE)
+df <- predict(dummies, df) %>% as.data.frame()
 
-# Normalize numeric columns (age, balance, duration, campaign, previous)
+# Normalizzazione delle variabili numeriche
 numeric_cols <- c("age", "balance", "duration", "campaign", "previous")
 df[numeric_cols] <- scale(df[numeric_cols])
 
-# Creare il modello di regressione logistica
-logistic_model <- glm(y ~ ., data = df, family = binomial())
+# Dividere i dati in train (80%) e test (20%)
+set.seed(42)
+trainIndex <- createDataPartition(df$y, p = 0.8, list = FALSE)
+train <- df[trainIndex, ]
+test <- df[-trainIndex, ]
+
+# Creare il modello logistico
+logistic_model <- glm(y ~ ., data = train, family = binomial())
 
 # Mostrare il sommario del modello
 summary(logistic_model)
 
+# Predizioni sul test set
+test$predicted_prob <- predict(logistic_model, newdata = test, type = "response")
+test$predicted_class <- ifelse(test$predicted_prob > 0.5, 1, 0)
+
+# Matrice di confusione
+conf_matrix <- table(Predicted = test$predicted_class, Actual = test$y)
+print(conf_matrix)
+
+# Calcolo di sensibilità, specificità e accuratezza
+sensitivity <- conf_matrix[2, 2] / sum(conf_matrix[, 2])
+specificity <- conf_matrix[1, 1] / sum(conf_matrix[, 1])
+accuracy <- sum(diag(conf_matrix)) / sum(conf_matrix)
+
+cat("Sensibilità:", sensitivity, "\n")
+cat("Specificità:", specificity, "\n")
+cat("Accuratezza:", accuracy, "\n")
+
+# Curva ROC e AUC
+roc_curve <- roc(test$y, test$predicted_prob)
+auc_value <- auc(roc_curve)
+plot(roc_curve, col = "blue", main = paste("Curva ROC - AUC:", round(auc_value, 2)))
+
+# Grafico della distribuzione di y rispetto alla durata della chiamata
+ggplot(test, aes(x = duration, fill = factor(y))) +
+  geom_histogram(bins = 30, alpha = 0.6, position = "identity") +
+  labs(title = "Distribuzione della durata della chiamata rispetto a y",
+       x = "Durata della chiamata", y = "Frequenza") +
+  scale_fill_manual(values = c("red", "green"), name = "y") +
+  theme_minimal()
